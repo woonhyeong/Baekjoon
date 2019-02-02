fdce2f5d8f4859f878884c1cfdef5ff6e001dc18
woonhyeong <tlsehddkql12@gmail.com>
2019-02-03 00:07:37 +0900

    Update 1008

---
+++ 1008/1008/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  1008
+//
+//  Created by 이운형 on 02/02/2019.
+//  Copyright © 2019 201302458. All rights reserved.
+//
+let input = readLine()!.split(separator: " ").map{Double($0)!}
+print(input[0]/input[1])
+
