2cef2d54696af97ca0f6c8c45f365db286cb8bd1
woonhyeong <tlsehddkql12@gmail.com>
2019-02-19 16:56:48 +0900

    Update 1019

--- 1019/1019/main.swift
+++ 1019/1019/main.swift
@@ -6,7 +6,47 @@
 //  Copyright © 2019 201302458. All rights reserved.
 //
 
-import Foundation
+var input = Int(readLine()!)!
+var result: [Int] = Array(repeating: 0, count: 10)
+var digit = 1
 
-print("Hello, World!")
+func decrease(_ num: Int) {
+    var temp = num
+    while temp > 0 {
+        result[temp%10] += digit
+        temp = temp/10
+    }
+}
+
+while input > 0 {
+    while input%10 != 9 && input>0 {
+        decrease(input)
+        input -= 1
+    }
+    
+    if input == 0 {
+        break
+    }
+    
+    for i in 0...9 {
+        result[i] += (input/10 + 1)*digit
+    }
+    
+    input /= 10
+    digit *= 10
+}
+
+if result[0] > 0 {
+    while digit > 0 {
+        result[0] -= digit/10
+        digit = digit/10
+    }
+}
+
+var output = ""
+for element in result {
+    output.append(String(element)+" ")
+}
+print(output)
+
 
