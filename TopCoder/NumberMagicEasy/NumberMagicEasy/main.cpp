44f11e0631c6678925df48c2ad30d3fd65314ea2
woonhyeong <tlsehddkql12@gmail.com>
2019-05-20 23:56:39 +0900

    Update TopCoder/NumberMagicEasy

---
+++ TopCoder/NumberMagicEasy/NumberMagicEasy/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  NumberMagicEasy
+//
+//  Created by 이운형 on 19/05/2019.
+//  Copyright © 2019 201302458. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+class NumberMagicEasy {
+public:
+    int theNumber(string answer) {
+        
+        return 0;
+    }
+};
+
+int main() {
+    
+}
