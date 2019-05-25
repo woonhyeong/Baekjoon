491b4571306479276ebf40a46b775316cabb3afe
woonhyeong <tlsehddkql12@gmail.com>
2019-05-25 22:23:23 +0900

    Update 17144

---
+++ 17144/17144/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  17144
+//
+//  Created by 이운형 on 25/05/2019.
+//  Copyright © 2019 201302458. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    int R, C, T;
+    
+}
